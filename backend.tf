770814672ae9d609ed30c90f58a3b297f740c905
SHabimana1 <sylvain.habimana@gmail.comgit config --global user.email sylvain.habimana@gmail.com>
2025-11-29 16:48:12 -0700

    code terraform

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "w7-sh-terr-bucket" # Replace with your bucket name
-    key          = "uat/terraform.tfstate"
+    #key          = "uat/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = false
